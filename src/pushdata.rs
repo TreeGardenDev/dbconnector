06ad078f8d27d086a69e9dd222a0fd146a167358
Kyle Baumgarten <baumdev@proton.me>
2023-04-03 23:33:34 -0400

    message

--- src/pushdata.rs
+++ src/pushdata.rs
@@ -74,8 +74,8 @@ pub fn read_csv(file: &String) -> std::result::Result<(), Box<dyn std::error::Er
         println!("Column Index: {}", column); 
         for result in rdr2.records() {
         let record = result?;
-        let id = record[column].to_string();
-        println!("{}", id);
+        let _id = record[column].to_string();
+        println!("{}", _id);
 
        // data.push(Data {
        //     id,
